994da1124de9cde4bb2360aec420d1458367bada
alvezandreluiz <alvezandreluiz@gmail.com>
2022-05-20 01:32:30 -0300

    First commit

---
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,13 @@
+package me.dio.academia.digital.repository;
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+
+   List<AvaliacaoFisica> findByAlunoId(Long id);
+}
